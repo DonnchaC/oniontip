9fdd2a733b39040ebaf3cfd3138b19321cc9dfbc
Donncha O'Cearbhaill <donncha@donncha.is>
2014-07-11 13:58:48 +0100

    Config option for log file location

--- oniontip/__init__.py
+++ oniontip/__init__.py
@@ -22,7 +22,7 @@ if not app.debug:
     app.logger.addHandler(mail_handler)
 
     from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('oniontip.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler = RotatingFileHandler(app.config['LOGGER_NAME'], 'a', 1 * 1024 * 1024, 10)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
